1e091183533f33686802c2fb48fa3e94f9195a21
SUSSEXDOWNS\480630 <480630@chrome.sussexdowns.ac.uk>
2018-11-29 14:38:24 +0000

    First commit

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMotor : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    //Gets a movement vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    // Run every physics iteration
+    void FixedUpdate()
+    {
+        PerformMovement();
+    }
+    //Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+
 }
+
