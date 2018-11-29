f86a8c5639eed8cec12ed33d68989dfebf7ae366
SUSSEXDOWNS\480630 <480630@chrome.sussexdowns.ac.uk>
2018-11-29 14:39:16 +0000

    Revert "First commit"
    This reverts commit 1e091183533f33686802c2fb48fa3e94f9195a21.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
-
     [SerializeField]
 
     private float speed = 5f;
@@ -19,15 +18,6 @@ public class PlayerController : MonoBehaviour {
         //Calculate Movement velocity as a 3D vector
         float _xMov = Input.GetAxisRaw("Horizontal");
         float _zMov = Input.GetAxisRaw("Vertical");
-
-        Vector3 _movHorizontal = transform.right* _xMov; //(x1, y0, z0)
-        Vector3 _movVertical = transform.forward* _zMov; //(x0, y1, z0)
-
-            //Final Movement vector 
-        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
-
-        //Apply movement
-        motor.Move(_velocity);
     
     }
 
--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,36 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class PlayerMotor : MonoBehaviour
-{
+public class PlayerMotor : MonoBehaviour {
 
-    private Vector3 velocity = Vector3.zero;
-
-    private Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-    //Gets a movement vector
-    public void Move(Vector3 _velocity)
-    {
-        velocity = _velocity;
-    }
-    // Run every physics iteration
-    void FixedUpdate()
-    {
-        PerformMovement();
-    }
-    //Perform movement based on velocity variable
-    void PerformMovement()
-    {
-        if (velocity != Vector3.zero)
-        {
-            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
-        }
-    }
-
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
-
