26014e44a0cd96a7cecc7de2296c908808800ba8
SUSSEXDOWNS\480630 <480630@chrome.sussexdowns.ac.uk>
2018-12-06 14:41:37 +0000

    it's my first commit after changing the game genre

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,52 +1,30 @@
-﻿using UnityEngine;
-[RequireComponent(typeof(PlayerMotor))]
-public class PlayerController : MonoBehaviour
-{
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    [SerializeField]
+public class PlayerController : MonoBehaviour {
 
-    private float speed = 5f;
-    [SerializeField]
-    private float lookSensitivity = 3f;
-    private PlayerMotor motor;
+    Camera cam;
 
-    void Start()
-    {
-        motor = GetComponent<PlayerMotor>();
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-    }
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                //Move our player to what we hit
 
-    void Update()
-    {
-        //Calculate Movement velocity as a 3D vector
-        float _xMov = Input.GetAxisRaw("Horizontal");
-        float _zMov = Input.GetAxisRaw("Vertical");
-
-        Vector3 _movHorizontal = transform.right * _xMov; //(x1, y0, z0)
-        Vector3 _movVertical = transform.forward * _zMov; //(x0, y1, z0)
-
-        //Final Movement vector 
-        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
-
-        //Apply movement
-        motor.Move(_velocity);
-
-        //Calculate rotation as a 3D vector (turning around)
-        float _yRot = Input.GetAxisRaw("Mouse X");
-
-        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
-
-        //Apply rotation
-        motor.Rotate(_rotation);
-
-        //Calculate camera rotation as a 3D vector (turning around)
-        float _xRot = Input.GetAxisRaw("Mouse Y");
-
-        Vector3 _cameraRotation = new Vector3(0f, _xRot, 0f) * lookSensitivity;
-
-        //Apply camera rotation
-        motor.RotateCamera(_cameraRotation);
-
-    }
-
+                //Stop focusing any objects
+            }
+        }
+	}
 }
